de350ad83ab47324fda808571ccdc40f6453bf59
Balraj <balraj.johal@phntms.com>
2024-09-25 23:59:13 +0100

    Remove prod URL

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,8 @@
-import { VERCEL_PROD_URL, VERCEL_URL } from '$env/static/private';
+import { VERCEL_URL } from '$env/static/private';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = () => {
 	return {
-		url: VERCEL_PROD_URL ?? VERCEL_URL ?? "http://localhost:5173",
+		url: VERCEL_URL ?? 'http://localhost:5173'
 	};
 };
